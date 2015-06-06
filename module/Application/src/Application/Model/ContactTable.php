b8d1dd8428af8cce5c7b75a42b220df7a3a1b264
Péter Balogh <peter.balogh@theory9.hu>
2015-06-06 17:00:17 +0200

    add db repository for contact entity

---
+++ module/Application/src/Application/Model/ContactTable.php
@@ -0,0 +1,62 @@
+<?php
+/**
+ * Application module
+ * 
+ * @package Application
+ * @author Péter Balogh <peter.balogh@theory9.hu>
+ */
+
+namespace Application\Model;
+
+use Zend\Db\TableGateway\AbstractTableGateway;
+use Zend\Db\TableGateway\Feature\FeatureSet;
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\ResultSet\ResultSet;
+use Zend\Db\ResultSet\HydratingResultSet;
+use Zend\Db\Sql\Sql;
+use Zend\Db\Sql\Select;
+use Zend\Db\Sql\Where;
+
+
+/**
+ * Repository for contact
+ * 
+ * {@inheritdoc}
+ */
+class ContactTable extends AbstractTableGateway
+{
+    /**
+     * @var string base table for TableGateway
+     */
+    protected $table = 'contact';
+
+    /**
+     * TableGateway constructor
+     * 
+     * @param Adapter $dbAdapter
+     * @param HydratingResultSet $resultSetPrototype 
+     */
+    public function __construct(
+        Adapter $dbAdapter, 
+        HydratingResultSet $resultSetPrototype,
+        FeatureSet $featureSet,
+        Translator $translator
+    ) {
+        $this->adapter = $dbAdapter;
+        $this->resultSetPrototype = $resultSetPrototype;
+
+        $this->initialize();
+    }
+
+    /**
+     * Retrive all contacts
+     * 
+     * @return ResultSet
+     */
+    public function fetchAll()
+    {
+        $rows = $this->select();
+
+        return $rows;
+    }
+}
