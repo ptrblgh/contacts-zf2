0619e6d3d1f8d5118725dd9b66be04a3306cee94
Péter Balogh <peter.balogh@theory9.hu>
2015-06-07 13:14:19 +0200

    add controller factory for category

---
+++ module/Application/src/Application/Factory/CategoryControllerFactory.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ * Application module
+ * 
+ * @package Application
+ * @author Péter Balogh <peter.balogh@theory9.hu>
+ */
+
+namespace Application\Factory;
+
+use Zend\ServiceManager\FactoryInterface;
+use Zend\ServiceManager\ServiceLocatorInterface;
+use Application\Controller\CategoryController;
+
+/**
+ * Factory for instantiating {@see Application\Controller\CategoryController}
+ */
+class CategoryControllerFactory implements FactoryInterface
+{
+    /**
+     * {@inheritDoc}
+     * 
+     * @param  ServiceLocatorInterface $serviceLocator
+     * @return Application\Controller\CategoryController
+     */
+    public function createService(ServiceLocatorInterface $serviceLocator)
+    {
+        $parentLocator = $serviceLocator->getServiceLocator();
+
+        $categoryTable = $parentLocator->get('category_table');
+        $categoryInputFilter = $parentLocator->get('category_input_filter');
+        $appConfig = $parentLocator->get('config');
+
+        return new CategoryController(
+            $categoryTable, 
+            $categoryInputFilter,
+            $appConfig
+        );
+    }
+}
